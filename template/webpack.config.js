b6dd022aaa3afdeada66a7e20a3052686bcf6b64
Quan2812 <69678127+Quan2812@users.noreply.github.com>
2025-09-30 09:27:20 +0700

    Fix template

---
+++ template/webpack.config.js
@@ -0,0 +1,17 @@
+// template/webpack.config.js
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'template',
+  filename: 'remoteEntry.js',
+  exposes: {
+    './routes': './src/app/app.routes.ts',
+  },
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: '^18.2.0' },
+    '@angular/common': { singleton: true, strictVersion: true, requiredVersion: '^18.2.0' },
+    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: '^18.2.0' },
+    'rxjs': { singleton: true, strictVersion: true, requiredVersion: '~7.8.0' },
+  },
+});
